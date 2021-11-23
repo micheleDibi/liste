0f1b9bc374482db57fe7779279da5620f0f38886
micheleDibi <94901756+micheleDibi@users.noreply.github.com>
2021-11-23 14:54:33 +0100

    upload liste

---
+++ lista.h
@@ -0,0 +1,133 @@
+#ifndef LISTA_H
+#define LISTA_H
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using std::ostream;
+
+template <class T, class P>
+class linearList{
+public:
+    typedef T valueType;
+    typedef P position;
+
+    virtual void createList() = 0;
+    virtual bool isEmpty()const = 0;
+    virtual valueType readList(position) const = 0;
+    virtual void writeList(position, valueType&) = 0;
+    virtual position _begin() const = 0;
+    virtual position _last() const = 0;
+    virtual bool _end(position) const = 0;
+    virtual position next(position) const = 0;
+    virtual position prec(position) const = 0;
+    virtual void insList(position, valueType&) = 0;
+    virtual void delList(position) = 0;
+
+    int listSize() const;
+    void invertireLista();
+    bool palindroma() const;                        //verifica se una lista è palindroma
+
+    //virtual void clearList();                               //svuota la lista
+    int numElements(position, position);            //calcola il numero di elementi presenti dalla pos x alla posizione y - accertarsi che x > y
+    //void exchangeElements(position, position);      //cambia la posizione del elemento in pos x con quello in posizione y - non è necessario che x > y
+    //void moveMinAndMax();                           //inserisce il minimo in prima posizione e il minimo in ultima posizione
+} ;
+
+template <class T, class P>
+int linearList<T, P>::listSize() const{
+    position pos = _begin();
+    int c = 0;
+
+    while(!_end(pos)) {
+        c++;
+        pos = next(pos);
+    }
+
+    return c;
+}
+
+template <class T, class P>
+void linearList<T, P>::invertireLista() {
+    position head, tail;
+    head = _begin();
+    tail = _last();
+
+    for (int i = 0; i < (listSize()/2); i++) {
+        valueType supportH = readList(head);
+        valueType supportT = readList(tail);
+        writeList(head, supportT);
+        writeList(tail, supportH);
+
+        tail = prec(tail);
+        head = next(head);
+    }
+}
+
+template <class T, class P>
+bool linearList<T, P>::palindroma() const {
+    if(!isEmpty()) {
+        position head = _begin(), tail = _last();
+
+        for(int i = 0; i < listSize(); i++) {
+            if(readList(head) != readList(tail)) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+template <class T, class P>
+int linearList<T, P>::numElements(linearList<T, P>::position x, linearList<T, P>::position y) {
+    /**
+        casi possibili di input:
+        . x > y -> distanza = counter
+        . x == y -> distanza = 0;
+        .y > x -> invertire le posizioni poi vedi 'x > y'
+    */
+
+    int counter = 0;
+
+    //implementare confronto tra posizioni di puntatori
+    if(y > x) {
+        linearList<T, P>::position pos = x;
+        x = y;
+        auto y = pos;
+    }
+
+    while(x != y) {
+        x = next(x);
+        counter++;
+    }
+
+    return counter;
+}
+
+template <class T, class P>
+ostream& operator<<(ostream& os, linearList<T, P> &l) {
+    typename linearList<T, P>::position pos;
+    pos = l._begin();
+    cout << "[";
+    while(!l._end(pos)) {
+        if(pos != l._begin()) {
+            cout << ", " << l.readList(pos);
+        } else {
+            cout << l.readList(pos);
+        }
+        pos = l.next(pos);
+    }
+    cout << "]" << endl;
+    return os;
+}
+
+template <class T, class P, typename N>
+void fillList(linearList<T, P> &l, N noc) {
+    while(noc > 0) {
+        l.insList(l._begin(), noc);
+        noc--;
+    }
+}
+#endif // LISTA_H
---
+++ listaVettore.h
@@ -0,0 +1,239 @@
+#ifndef LISTAVETTORE_H
+#define LISTAVETTORE_H
+#include "lista.h"
+
+#define MAX 10
+
+template <class T>
+class vectorList : public linearList<T, int> {
+public:
+    typedef typename linearList<T, int>::valueType valueType;
+    typedef typename linearList<T, int>::position position;
+
+    //costruttori
+    vectorList();
+    vectorList(int);
+
+    //distruttore
+    ~vectorList();
+
+    //metodi
+    void createList();
+    bool isEmpty() const;
+    valueType readList(position) const;
+    void writeList(position, valueType&);
+    position _begin() const;
+    position _last() const;
+    bool _end(position) const;
+    position next(position) const;
+    position prec(position) const;
+    void insList(position, valueType&);
+    void delList(position);
+    void fillList(valueType&);
+
+    vectorList<T>& operator=(const vectorList<T> &);
+    bool operator==(const vectorList<T> &l);
+    bool operator!=(const vectorList<T> &l);
+
+private:
+    valueType *elements;
+    int lenght;
+    int dim;
+
+};
+
+//operazione di copia
+template <class T>
+vectorList<T>& vectorList<T>::operator=(const vectorList<T> &l) {
+    if(this != &l) {
+        dim = l.dim;
+        lenght = l.lenght;
+        delete this -> elements;
+        this -> elements = new T[dim];
+        for (int i = 0; i < dim; i++)
+            elements[i] = l.elements[i];
+    }
+
+    return *this;
+}
+
+//operazione di confronto
+template <class T>
+bool vectorList<T>::operator==(const vectorList<T> &l) {
+    if(this->lenght != l.lenght) {
+        return false;
+    }
+    for (int i = 0; i < dim; i++) {
+        if(elements[i] != l.elements[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+template <class T>
+bool vectorList<T>::operator!=(const vectorList<T> &l) {
+    if(this->lenght != l.lenght) {
+        return true;
+    }
+
+    for (int i = 0; i < dim; i++) {
+        if(elements[i] != l.elements[i]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+//realizzazione costruttori
+template <class T>
+vectorList<T>::vectorList() {
+    dim = MAX;
+    this->createList();
+}
+
+template <class T>
+vectorList<T>::vectorList(int dimension) {
+    dim = dimension;
+    this->createList();
+}
+
+template <class T>
+vectorList<T>::~vectorList() {
+    delete[] elements;
+}
+
+//realizzazione metodi
+template <class T>
+void vectorList<T>::createList() {
+    elements = new T[dim];
+    lenght = 0;
+}
+
+template <class T>
+bool vectorList<T>::isEmpty() const{
+    if(lenght == 0) {
+        return true;
+    } else return false;
+}
+
+template <class T>
+typename vectorList<T>::valueType vectorList<T>::readList(vectorList<T>::position pos) const{
+    if((pos > 0) && (pos < lenght+1)) {
+        return elements[pos - 1];
+    }
+}
+
+template <class T>
+void vectorList<T>::writeList(vectorList<T>::position pos, vectorList<T>::valueType &e) {
+    if((pos > 0) && (pos < lenght+1)) {
+        elements[pos - 1] = e;
+    }
+}
+
+template <class T>
+typename vectorList<T>::position vectorList<T>::_begin() const{
+    return 1;
+}
+
+template <class T>
+typename vectorList<T>::position vectorList<T>::_last() const {
+    return lenght;
+}
+
+template <class T>
+bool vectorList<T>::_end(vectorList<T>::position pos) const{
+    if ((pos > 0) && (pos <= lenght + 1)) {
+        if(pos == lenght + 1)
+            return true;
+        else
+            return false;
+    }
+}
+
+template <class T>
+typename vectorList<T>::position vectorList<T>::next(vectorList<T>::position pos) const{
+    if((pos > 0) && (pos < lenght + 1))
+        return pos + 1;
+    else return pos;
+}
+
+template <class T>
+typename vectorList<T>::position vectorList<T>::prec(vectorList<T>::position pos) const{
+    if((pos > 1) && (pos < lenght + 1))
+        return (pos - 1);
+    else return pos;
+}
+
+template <class T>
+void vectorList<T>::insList(vectorList<T>::position pos, vectorList<T>::valueType &e) {
+    if((pos > 0) && (pos <= lenght + 1)) {
+        for (int i = lenght; i >= pos; i--) {
+            elements[i] = elements[i - 1];
+        }
+        elements[pos - 1] = e;
+        lenght++;
+    }
+}
+
+template <class T>
+void vectorList<T>::delList(vectorList<T>::position pos) {
+    if((pos > 0) && (pos < lenght+1)) {
+        for (int i = pos-1; i < lenght-1; i++) {
+            elements[i] = elements[i + 1];
+        }
+        lenght = lenght - 1;
+    }
+}
+
+
+template <class T>
+void vectorList<T>::fillList(vectorList<T>::valueType &noc) {
+    while(noc > 0) {
+        insList(_begin(), noc);
+        noc--;
+        lenght++;
+    }
+}
+
+#endif // LISTAVETTORE_H
+
+/* #MAIN#
+    int number = 1414;
+    vectorList<int> listOne;                                                                //iniziliazzazione lista con costruttore di default
+    fillList(listOne, 10);                                                                  //inserimento di valori 1-10 nella lista
+
+    vectorList<int>::position pos;
+    pos = listOne.next(listOne.next(listOne._begin()));                                     //recupero posizione 3 della lista, partendo dall'inizio della lista e andando due volte avanti
+    listOne.writeList(pos, number);                                                         //sovrascrittura dell'elemento in posizione 3 con 'number'
+
+    pos = listOne.prec(pos);                                                                //torno indietro di una posizione
+    cout << "A-valore presente in posizione 2: " << listOne.readList(pos) << endl;
+
+    listOne.delList(listOne._begin());                                                      //eliminazione del primo elemento
+
+    vectorList<int> listTwo(20);                                                            //inizializzazione lista con costruttore di default che imposta la dimensione della lista, in questo caso '20'
+    fillList(listTwo, 20);                                                                  //inserimento di valori 1-20 nella lista
+
+    listTwo.invertireLista();                                                               //invertire l'ordine delle posizioni della lista
+
+    if(listOne != listTwo) {                                                                //controllo che le due liste siano differenti
+        cout << "E-le due liste sono differenti" << endl;
+    } else {
+        cout << "E-le due liste sono uguali" << endl;
+    }
+
+    if(!(listOne.isEmpty()) && !(listTwo.isEmpty())) {                                      //controllo che la lista non sia vuota
+        cout << "B-lista uno completa:" << listOne;
+        cout << "B-lista due completa:" << listTwo;
+
+        listTwo = listOne;                                                                  //utilizzo del costruttore di copia per copiare 'listOne' in 'listTwo'
+
+        if(listOne == listTwo) {                                                            //utilizzo del costruttore di confronto per le due liste
+            cout << "le due liste sono uguali" << endl;
+            cout << "C-lista uno completa:" << listOne;
+            cout << "C-lista due completa:" << listTwo;
+        } else cout << "le due liste sono diverse" << endl;
+    }
+    else cout << "le due liste sono vuote" << endl;
+*/
